2af01f6516c3d6e5b194331ac44176f947a8ca46
markusbuchholz <markus.x.buchholz@gmail.com>
2020-06-18 11:42:32 +0200

    update vec

--- vector_dev.cpp
+++ vector_dev.cpp
@@ -34,54 +34,55 @@ int main()
     rng.seed(std::random_device()());
     std::uniform_int_distribution<int> dist(0, 10);
 
-    // std::vector<std::vector<int>> vec;
-    // std::vector<int> temp_vec;
-    // ;
-    // auto ptr_vec2 = vec.begin();
+  std::vector<std::vector<int>> vec;
+    
 
-    // for (int i = 0; i < ROW; i++)
-    // {
-    //     for (int j = 0; j < COL; j++)
-    //     {
-    //         temp_vec.push_back(dist(rng));
-    //     }
-    //     vec.push_back(temp_vec);
-    // }
+    for (int i = 0; i < ROW; i++)
+    {
+    std::vector<int> temp_vec;
+        for (int j = 0; j < COL; j++)
+        {
+            temp_vec.push_back(dist(rng));
+        }
+        vec.push_back(temp_vec);
+    }
 
-    // // Displaying the 2D vector 
-    // for (int i = 0; i < vec.size(); i++) { 
-    //     for (int j = 0; j < vec[i].size(); j++) 
-    //         std::cout << vec[i][j] << " "; 
-    //     std::cout << std::endl; 
-    // } 
-
-    // Initializing the vector of vectors 
-    std::vector<std::vector<int> > vec; 
-  
-    // Elements to insert in column 
-    int num = 10; 
-  
-    // Inserting elements into vector 
-    for (int i = 0; i < ROW; i++) { 
-        // Vector to store column elements 
-        std::vector<int> v1; 
-  
-        for (int j = 0; j < COL; j++) { 
-            v1.push_back(num); 
-            num += 5; 
-        } 
-  
-        // Pushing back above 1D vector 
-        // to create the 2D vector 
-        vec.push_back(v1); 
-    } 
-  
     // Displaying the 2D vector 
     for (int i = 0; i < vec.size(); i++) { 
         for (int j = 0; j < vec[i].size(); j++) 
             std::cout << vec[i][j] << " "; 
         std::cout << std::endl; 
     } 
+
+
+
+    // // Initializing the vector of vectors 
+    // std::vector<std::vector<int> > vec; 
+  
+    // // Elements to insert in column 
+    // int num = 10; 
+  
+    // // Inserting elements into vector 
+    // for (int i = 0; i < ROW; i++) { 
+    //     // Vector to store column elements 
+    //     std::vector<int> v1; 
+  
+    //     for (int j = 0; j < COL; j++) { 
+    //         v1.push_back(num); 
+    //         num += 5; 
+    //     } 
+  
+    //     // Pushing back above 1D vector 
+    //     // to create the 2D vector 
+    //     vec.push_back(v1); 
+    // } 
+  
+    // // Displaying the 2D vector 
+    // for (int i = 0; i < vec.size(); i++) { 
+    //     for (int j = 0; j < vec[i].size(); j++) 
+    //         std::cout << vec[i][j] << " "; 
+    //     std::cout << std::endl; 
+    //} 
 
 
 
